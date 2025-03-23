3b4e895b4f6b180957357fb677d2d71577df1cfb
Damian Niejadlik <omikix@gmail.com>
2025-03-23 21:57:33 +0100

    lepiej

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, GodzinyPracy, WniosekUrlopowy
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['username', 'email', 'role', 'dzial', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Dodatkowe informacje', {'fields': ('role', 'dzial')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Dodatkowe informacje', {'fields': ('role', 'dzial')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(GodzinyPracy)
+admin.site.register(WniosekUrlopowy)
